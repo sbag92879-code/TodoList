b892b068f0eace4044cc7d2bf9efbc64d3ffb253
sbag92879-code <sbag92879@gmail.com>
2026-06-29 01:38:56 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+
+import './App.css'
+import { useState } from 'react'
+
+import Editor from './assets/components/Editor'
+import Header from './assets/components/Header'
+import List from './assets/components/List'
+
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    date: new Date().getTime(),
+  },
+
+];
+
+function App() {
+  const [todos, setTodos] = useState(mockData);
+
+  return (
+
+    <div className='App'>
+      <Header />
+      <Editor />
+      <List />
+    </div>
+  );
+}
+
+export default App
