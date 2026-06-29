8cd93df63aed89c023a05d6b3e28d94bd08f5994
sbag92879-code <sbag92879@gmail.com>
2026-06-29 20:24:42 +0900

    .

--- src/assets/components/List.jsx
+++ src/assets/components/List.jsx
@@ -1,12 +1,32 @@
 import "./List.css"
 import TodoItem from "./TodoItem";
+import { useState } from "react";
 
 
 const List = ({ todos }) => {
+
+    const [search, setSearch] = useState("");
+
+    const onChangeSearch = (e) => {
+        setSearch(e.targte.value);
+    };
+
+    const getFilteredData = () => {
+        if(search === ""){
+            return todos;
+        }
+        return todos.filter((todo) => 
+            todo.content.includes(search)
+        );
+    }
+
+    const filteredTodos = getFilteredData();
+
     return (
         <div className="List">
             <h4>Todo List 🌱</h4>
-            <input placeholder="검색어를 입력하세요" />
+            <input value={search} onChange={onChangeSearch}
+            placeholder="검색어를 입력하세요" />
             <div className="todos_wrapper">
               {todos.map((todo) => {
                 return <TodoItem key={todo.id}{...todo}/>;
