11df30520bbf4fc0be8d6df1b74820c562d743d3
sbag92879-code <sbag92879@gmail.com>
2026-06-29 16:39:51 +0900

    .

--- src/assets/components/Editor.jsx
+++ src/assets/components/Editor.jsx
@@ -1,22 +1,39 @@
 import "./Editor.css"
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const Editor = ({ onCreate }) => {
+
+    const [content, setContent] = useState("");
+    const contentRef = useRef();
 
     const onChangeContent = (e) => {
         setContent(e.target.value);
     };
 
-    const [content, setContent] = useState("");
+
+    const onKeydown = (e) => {
+        if (e.KeyCode === 13){
+            onsubmit();
+        }
+    };
+
 
     const onsubmit = () => {
-        onCreate()
+        if (content === "") {
+            contentRef.current.focus();
+            return;
+        }
+        onCreate(content);
+        setContent("");
     };
 
 
     return (
         <div className="Editor">
-            <input value={content}
+            <input
+                onKeyDown={onKeydown}
+                ref={contentRef}
+                value={content}
                 onChange={onChangeContent}
                 placeholder="새로운 Todo..." />
             <button onClick={onsubmit}>추가</button>
