991a3b16900ed483fa1257be16c2cd350ec2cba9
sbag92879-code <sbag92879@gmail.com>
2026-06-29 20:10:06 +0900

    .

--- src/assets/components/List.jsx
+++ src/assets/components/List.jsx
@@ -2,15 +2,15 @@ import "./List.css"
 import TodoItem from "./TodoItem";
 
 
-const List = () => {
+const List = ({ todos }) => {
     return (
         <div className="List">
             <h4>Todo List 🌱</h4>
             <input placeholder="검색어를 입력하세요" />
             <div className="todos_wrapper">
-                <TodoItem />
-                <TodoItem />
-                <TodoItem />
+              {todos.map((todo) => {
+                return <TodoItem key={todo.id}{...todo}/>;
+              })}
             </div>
         </div>
 
