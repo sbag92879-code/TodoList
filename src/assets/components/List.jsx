a83b45711ac930cd2bbc349ad89197e977c09a1f
sbag92879-code <sbag92879@gmail.com>
2026-06-30 20:22:20 +0900

    .

--- src/assets/components/List.jsx
+++ src/assets/components/List.jsx
@@ -3,22 +3,21 @@ import TodoItem from "./TodoItem";
 import { useState } from "react";
 
 
-const List = ({ todos }) => {
-
+const List = ({ todos, onUpdate }) => {
     const [search, setSearch] = useState("");
 
     const onChangeSearch = (e) => {
-        setSearch(e.targte.value);
+        setSearch(e.target.value);
     };
 
     const getFilteredData = () => {
-        if(search === ""){
+        if (search === "") {
             return todos;
         }
-        return todos.filter((todo) => 
-            todo.content.includes(search)
+        return todos.filter((todo) =>
+            todo.content.toLowerCase().includes(search.toLocaleLowerCase())
         );
-    }
+    };
 
     const filteredTodos = getFilteredData();
 
@@ -26,11 +25,11 @@ const List = ({ todos }) => {
         <div className="List">
             <h4>Todo List 🌱</h4>
             <input value={search} onChange={onChangeSearch}
-            placeholder="검색어를 입력하세요" />
+                placeholder="검색어를 입력하세요" />
             <div className="todos_wrapper">
-              {todos.map((todo) => {
-                return <TodoItem key={todo.id}{...todo}/>;
-              })}
+                {filteredTodos.map((todo) => {
+                    return <TodoItem key={todo.id}{...todo} onUpdate={onUpdate}/>;
+                })}
             </div>
         </div>
 
--- src/assets/components/TodoItem.jsx
+++ src/assets/components/TodoItem.jsx
@@ -1,10 +1,20 @@
 import "./TodoItem.css"
 
 
-const TodoItem = ({id, isDone, content, date}) =>{
+const TodoItem = ({id, isDone, content, date, onUpdate,}) =>{
+
+
+    const onChangeCheckbox = () => {
+        onUpdate(id);
+
+    };
+
     return (
+
     <div className="TodoItem">
-        <input readOnly checked={isDone}
+        <input 
+        onChange={onChangeCheckbox}
+        readOnly checked={isDone}
         type="checkbox" />
         <div className="content">{content}</div>
         <div className="date">{new Date(date).toLocaleDateString()}</div>
